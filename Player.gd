282c9e3d0325c353df9f41f54c8539ec3987c527
Evv3n <eivind.andreas.hans1@gmail.com>
2022-12-09 10:26:19 +0100

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var friction = 15
+var acceleration = 15
+
+
+func _physics_process(delta):
+	apply_gravity()
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input.x == 0:
+		apply_friction()
+	else:
+		apply_acceleration(input.x)
+		
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = -100
+	velocity = move_and_slide(velocity)
+	
+func apply_gravity():
+	velocity.y += 4
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, friction)
+	
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, 50 * amount, acceleration)
+	
